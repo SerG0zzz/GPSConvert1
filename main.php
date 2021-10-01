2c972d9a85980d8a14581b94cafc361d9e258c67
harlam <harlamov.nikolay@yandex.ru>
2021-10-01 18:43:37 +0300

    draft: txt provider

--- main.php
+++ main.php
@@ -1,64 +1,13 @@
 #!/usr/bin/env php
 <?php
 
-use Lesson\GpsConverter\Provider\LocationFakeProvider;
+use Lesson\GpsConverter\Provider\LocationTxtProvider;
 
 require_once 'vendor/autoload.php';
 
-$provider = new LocationFakeProvider();
+$provider = new LocationTxtProvider();
+$provider->load('./src/data/coordinates.txt');
 
-var_dump($provider->getLocations());
-exit;
-/*$str = "55°27'37,41";
-
-$degree = (integer)substr($str, 0, 2);
-$minute = (integer)substr($str, 4, 2);
-$second = (integer)substr($str, 7, 2);
-$part_s = (integer)substr($str, 10, 2);
-
-echo $str . "<br>";
-$coord = $degree + $minute / 60;
-$coord = $coord + $second / 3600;
-$coord = $coord + ($part_s / 100) / 3600;
-//echo $coord;
-*/
-
-//$file = file_get_contents('./coord.txt', true);
-//$out = '';
-//$array = explode("\n", $file);
-//foreach($array as $ar)
-//{
-//    if (strpos($ar, '°') !== false) {
-//        $pos = strpos($ar, "\t", 0);
-//        $name = substr($ar, 0, $pos);
-//        $degree1 = (integer)substr($ar, $pos, 4);
-//        $pos += 5;
-//        $minute1 = (integer)substr($ar, $pos, 4);
-//        $pos += 3;
-//        $second1 = (integer)substr($ar, $pos, 4);
-//        $pos += 3;
-//        $part_s1 = (integer)substr($ar, $pos, 4);
-//        $pos += 3;
-//        $degree2 = (integer)substr($ar, $pos, 4);
-//        $pos += 5;
-//        $minute2 = (integer)substr($ar, $pos, 4);
-//        $pos += 3;
-//        $second2 = (integer)substr($ar, $pos, 4);
-//        $pos += 3;
-//        $part_s2 = (integer)substr($ar, $pos, 4);
-//        $coord1 = $degree1 + $minute1 / 60;
-//        $coord1 = $coord1 + $second1 / 3600;
-//        $coord1 = $coord1 + ($part_s1 / 100) / 3600;
-//        $coord2 = $degree2 + $minute2 / 60;
-//        $coord2 = $coord2 + $second2 / 3600;
-//        $coord2 = $coord2 + ($part_s2 / 100) / 3600;
-//        //echo $ar . " >> " . $coord1 . " " . $coord2 . "<br>";
-//        $out .= "<Placemark id=\"0C69AD0E2E1D5C1207CD\">
-//		<name>скв_" . $name . "</name>
-//		<Point>
-//			<coordinates>" . $coord1 . "," . $coord2 . ",0</coordinates>
-//		</Point>
-//	</Placemark>";
-//    }
-//}
-//file_put_contents('./out_placemark.txt', $out);
+foreach ($provider->getLocations() as $location) {
+    printf("Location: '%s' has %u bores\n", $location->getName(), count($location->getBores()));
+}
--- src/Provider/LocationTxtProvider.php
+++ src/Provider/LocationTxtProvider.php
@@ -2,38 +2,91 @@
 
 namespace Lesson\GpsConverter\Provider;
 
+use Exception;
+use Lesson\GpsConverter\Entity\Bore;
+use Lesson\GpsConverter\Entity\Location;
+
 class LocationTxtProvider
 {
-    public function getBores()
+    private array $locations = [];
+
+    /**
+     * @throws Exception
+     */
+    public function load(string $path): self
     {
-        $array = explode("\n", file_get_contents('./coord.txt', true));
-        foreach($array as $ar)
-        {
-        if (strpos($ar, '°') !== false) {
-            $pos = strpos($ar, "\t", 0);
-            $name = substr($ar, 0, $pos);
-            $degree1 = (integer)substr($ar, $pos, 4);
-            $pos += 5;
-            $minute1 = (integer)substr($ar, $pos, 4);
-            $pos += 3;
-            $second1 = (integer)substr($ar, $pos, 4);
-            $pos += 3;
-            $part_s1 = (integer)substr($ar, $pos, 4);
-            $pos += 3;
-            $degree2 = (integer)substr($ar, $pos, 4);
-            $pos += 5;
-            $minute2 = (integer)substr($ar, $pos, 4);
-            $pos += 3;
-            $second2 = (integer)substr($ar, $pos, 4);
-            $pos += 3;
-            $part_s2 = (integer)substr($ar, $pos, 4);
-            $coord1 = $degree1 + $minute1 / 60;
-            $coord1 += $second1 / 3600;
-            $coord1 += ($part_s1 / 100) / 3600;
-            $coord2 = $degree2 + $minute2 / 60;
-            $coord2 += $second2 / 3600;
-            $coord2 += ($part_s2 / 100) / 3600;
+        $lines = explode("\n", file_get_contents($path));
+        $currentLineIsLocation = true;
+        $currentLocation = null;
+        $result = [];
+        foreach ($lines as $line) {
+            if ($line === '') {
+                if ($currentLocation instanceof Location) {
+                    $result[] = $currentLocation;
+                    $currentLineIsLocation = true;
+                    $currentLocation = null;
+                }
+                continue;
+            }
+
+            if (true === $currentLineIsLocation) {
+                $currentLocation = new Location($line);
+                $currentLineIsLocation = false;
+            } else {
+                $exploded = explode("\t", $line);
+
+                if (count($exploded) !== 3) {
+                    throw new Exception('Bad line format');
+                }
+
+                if (false === is_numeric($exploded[0])) {
+                    throw new Exception('Bore number is wrong');
+                }
+
+                if ($currentLocation === null) {
+                    throw new Exception('Location parsing error');
+                }
+
+                $currentLocation->addBore(
+                    new Bore(
+                        (int)$exploded[0],
+                        $this->stringToCoordinate($exploded[1]),
+                        $this->stringToCoordinate($exploded[2])
+                    )
+                );
             }
         }
+        $this->locations = $result;
+        return $this;
+    }
+
+    /**
+     * @return Location[]
+     */
+    public function getLocations(): array
+    {
+        return $this->locations;
+    }
+
+    protected function stringToCoordinate(string $coordinate): float
+    {
+        $prepared = str_replace(' ', '', $coordinate);
+        $prepared = str_replace(',', '.', $prepared);
+
+        /** @see https://www.designcise.com/web/tutorial/is-there-a-way-to-use-multiple-delimiters-with-the-php-explode-function */
+        $exploded = preg_split("/[°'\"]/", $prepared, -1, PREG_SPLIT_NO_EMPTY);
+
+        if (
+            (count($exploded) !== 3) ||
+            (false === (is_numeric($exploded[0]) && is_numeric($exploded[1]) && is_numeric($exploded[2])))
+        ) {
+            throw new Exception('Bad coordinates format. Use format: 55°27\'37,41"');
+        }
+
+        $degree = floatval($exploded[0]);
+        $minute = floatval($exploded[1]);
+        $second = floatval($exploded[2]);
+
+        return $degree + ($minute / 60) + ($second / 3600);
     }
 }
